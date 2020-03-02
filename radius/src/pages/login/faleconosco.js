d42f03104ecbf1bd12892c6c1161075d50ac9eb5
Belle Nerissa <belizeu@sga.pucminas.br>
2020-03-02 19:02:38 -0300

    alterações 2

--- radius/src/pages/login/faleconosco.js
+++ radius/src/pages/login/faleconosco.js
@@ -1,16 +1,32 @@
 import React from "react";
 import styles from "./faleconosco.module.scss";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Form, Grid , FormGroup } from "react-bootstrap";
 
-function FaleConosco()
-{
-    return(
-    <Container className={styles.Faleconosco}>
-        <Row className={styles.Header}>
+
+function FaleConosco() {
+  return (
+    <Container className={styles.faleconoscoContainer}>
+    <div className= {styles.Header}>
+      <Row className={styles.textTitle}>
         <p className={styles.title1}>Fale conosco</p>
-        <p className={styles.title2}>Insira seus dados nos campos abaixo.</p>
-        </Row>
+        <p className={styles.title2}>Insira seus dados nos campor abaixo.</p>
+      </Row>
+      </div>
+      <Form>
+        <Form.Row>
+          <Form.Group as={Col} md="2" controlId="formGridNome">
+            <Form.Label>Nome</Form.Label>
+            <Form.Control type="nome"/>
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="formGridEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control type="email"/>
+          </Form.Group>
+        </Form.Row>
+      </Form>
     </Container>
-    );
+
+  );
 }
 export default FaleConosco;
