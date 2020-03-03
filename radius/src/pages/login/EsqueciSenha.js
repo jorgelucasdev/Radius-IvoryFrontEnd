b90e4aaae6a4e6f0644b759f08b7a3de0ec136b5
Jean Araujo <jean.araujo@ivoryit.com.br>
2020-03-03 14:56:08 -0300

    Novos ajustes com BootStrap agora implementado

--- radius/src/pages/login/EsqueciSenha.js
+++ radius/src/pages/login/EsqueciSenha.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import styles from "./EsqueciSenha.module.scss";
 import { Container, Row, Col } from "react-bootstrap";
+import {faAngleLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 export default class EsqueciSenha extends Component {
     render() {
         return (
             <Container className={styles.esqueciSenha}>
                 <header className={styles.header}>
+                    <FontAwesomeIcon icon={faAngleLeft} className={styles.leftAngle}/>
                     <p className={styles.title}>Esqueci minha senha</p>
                     <p className={styles.subtitle}>Insira seus dados nos campos abaixo.</p>
                 </header>
