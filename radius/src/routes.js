77735742d9c83a3e55112942747579ee09859922
Arthur Gabriel <arthurgmm516@hotmail.com>
2020-03-04 15:52:00 -0300

    segunda rota implementada

--- radius/src/routes.js
+++ radius/src/routes.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Login from './pages/login/login';
 import EsqueciSenha from './pages/login/EsqueciSenha';
+import FaleConosco from './pages/login/faleconosco';
 
 const Routes = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={Login} />
             <Route path="/esquecisenha/" component={EsqueciSenha} />
+            <Route path="/faleconosco/" component={FaleConosco} />
         </Switch>
     </BrowserRouter>
 ); 
