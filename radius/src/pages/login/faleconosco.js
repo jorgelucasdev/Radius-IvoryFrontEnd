b4c85241b573ccfe57dee1addec5e5d7fb877d91
Belle Nerissa <belizeu@sga.pucminas.br>
2020-03-05 14:13:05 -0300

    fale conosco pré-implementação alterações rotas

--- radius/src/pages/login/faleconosco.js
+++ radius/src/pages/login/faleconosco.js
@@ -5,8 +5,7 @@ import {
   Row,
   Col
 } from "react-bootstrap";
-import {faAngleLeft } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 function FaleConosco() {
   return (
