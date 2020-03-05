ff6419bb26b019dc6183465f413472efadf1dfa5
Arthur Gabriel <arthurgmm516@hotmail.com>
2020-03-05 15:08:29 -0300

    recorte login/esqueci/fale finalizado

--- radius/src/pages/login/faleconosco.js
+++ radius/src/pages/login/faleconosco.js
@@ -9,77 +9,46 @@ import {
 function FaleConosco() {
   return (
     <div className={styles.faleconosco}>
-<<<<<<< HEAD
-      <Row className={styles.text}>
-        <p className={styles.title}>Fale conosco</p>
-      </Row>
-=======
                 <header className={styles.header}>
                     <p className={styles.title}>Fale conosco</p>
                     <p className={styles.subtitle}>Insira seus dados nos campos abaixo.</p>
                 </header>
->>>>>>> faleconosco
 
       <div className={styles.form}>
         <Row>
           <Col>
-            <Row>
               <label className={styles.label}>Nome:</label>
-            </Row>
-            <Row>
               <input type ="text" className={styles.input}></input>
-            </Row>
           </Col>
 
-<<<<<<< HEAD
-          <Col>
-=======
           {/* <Col md={{offset: 1}}> */}
           <Col className={styles.col2}>
->>>>>>> faleconosco
-            <Row>
               <label className={styles.label}>Email:</label>
-            </Row>
-            <Row>
               <input type ="text" className={styles.input}></input>
-            </Row>
           </Col>
         </Row>
 
         <Row>
           <Col>
-            <Row>
               <label className={styles.label}>
                 Nome da Empresa:
               </label>
-            </Row>
-            <Row>
               <input type ="text" className={styles.input}></input>
-            </Row>
           </Col>
 
           <Col className={styles.col2}>
-            <Row>
               <label className={styles.label}>Assunto:</label>
-            </Row>
-            <Row>
               <input type ="text" className={styles.input}></input>
-            </Row>
           </Col>
         </Row>
         <Row>
           <Col>
-            <Row>
+
               <label className={styles.label}>Mensagem:</label>
-            </Row>
-            <Row>
               <textarea className={styles.inputMensagem}></textarea>
-            </Row>
           </Col>
         </Row>
-        <Row>
           <button className={styles.btnEnviar}>ENVIAR</button>
-        </Row>
       </div>
     </div>
   );
