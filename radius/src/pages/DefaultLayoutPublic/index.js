689c2cfe2883f609d19581fc0a494f8720d26279
Jean Araujo <jean.araujo@ivoryit.com.br>
2020-03-09 16:58:38 -0300

    configurações rotas

--- radius/src/pages/DefaultLayoutPublic/index.js
+++ radius/src/pages/DefaultLayoutPublic/index.js
@@ -11,58 +11,41 @@ import LogoIvory from "../../assets/images/logoIvory.png";
 import styles from "./index.module.scss";
 
 function DefaultLayoutPublic(props) {
-    const { routes } = props;
+  const { route } = props;
 
-    return (
-        <div className={styles.autenticacao}>
-            <Container className={styles.container}>
-                <Col md={8} className={styles.form}>
-                    <Col sm={8}>
+  console.log(props);
 
-                        <header className={styles.header}>
-                            <a href="javascript:history.back()">
-                                <FontAwesomeIcon icon={faAngleLeft} className={styles.icon} />
-                            </a>
+  return (
+    <div className={styles.autenticacao}>
+      <Container className={styles.container}>
+        <Col md={8} className={styles.form}>
+          <Col sm={8}>
+            <header className={styles.header}>
+              <Link to={"/login"}>
+                <FontAwesomeIcon icon={faAngleLeft} className={styles.icon} />
+              </Link>
 
-                            <img src={Radius} className={styles.imgRadius} />
-                        </header>
+              <img src={Radius} className={styles.imgRadius} />
+            </header>
 
-
-                        <BrowserRouter>
-                            <Suspense
-                                fallback={<main>Carregando...</main>}
-                            >
-                                <Switch>
-                                    {routes.map((route, index) => {
-                                        return route.component ? (
-                                            <Route
-                                                key={index}
-                                                path={route.path}
-                                                exact={route.exact}
-                                                render={props => <route.component {...props} />}
-                                            />
-                                        ) : null;
-                                    })}
-                                </Switch>
-                            </Suspense>
-                        </BrowserRouter>
-
-
-
-
-                    </Col>
-
-
-                </Col>
-                <footer className={styles.footer}>
-                    <img src={LogoIvory} className={styles.logoFooter} />
-                    <p>Produto desenvolvido por IvoryIt</p>
-                </footer>
-            </Container>
-        </div>
-
-
-    );
+            <Suspense fallback={<main>Carregando...</main>}>
+              {route.component ? (
+                <Route
+                  path={route.path}
+                  exact={route.exact}
+                  render={props => <route.component {...props} />}
+                />
+              ) : null}
+            </Suspense>
+          </Col>
+        </Col>
+        <footer className={styles.footer}>
+          <img src={LogoIvory} className={styles.logoFooter} />
+          <p>Produto desenvolvido por IvoryIt</p>
+        </footer>
+      </Container>
+    </div>
+  );
 }
 
 export default DefaultLayoutPublic;
