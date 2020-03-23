c3766b611fd6a9d411dc3b8ef019c887dcdcbb08
Jean Araujo <jean.araujo@ivoryit.com.br>
2020-03-23 16:52:10 -0300

    NovoUsuario, label implementada

--- radius/src/pages/Cadastro/CadastroUsuarios.js
+++ radius/src/pages/Cadastro/CadastroUsuarios.js
@@ -3,13 +3,13 @@ import styles from "./CadastroClientes.module.scss";
 import "react-datepicker/dist/react-datepicker.css";
 import "styled-components";
 import "./AuxStyles.css";
-
+import NovoUsuario from './NovoUsuario';
 const CadastroUsuarios = props => {
   
   return (
     <div className={styles.principal}>
       <h1>Home > Cadastro > Usuário</h1>
-        
+        <NovoUsuario/>
     </div>
   );
 };
